f0be7381225fd6a9c3be131b028b94b2ad66c41e
ntscas <ntscas@gmail.com>
2026-07-28 16:32:42 +0900

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,30 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
-    return {
-      define: {
-        // This is just generic value for the GEMINI API key.
-        // This is not used at all, and can be ignored!
-        'process.env.API_KEY' : JSON.stringify('api-key-this-is-not-used-can-be-ignored!'),
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    // 🎯 GitHub Pages 레포지토리 이름 지정 (404 렌더링 에러 해결)
+    base: '/tax-ai/',
+
+    define: {
+      // This is just generic value for the GEMINI API key.
+      // This is not used at all, and can be ignored!
+      'process.env.API_KEY': JSON.stringify('api-key-this-is-not-used-can-be-ignored!'),
+    },
+    server: {
+      proxy: {
+        // Target your Node.js backend
+        '/api-proxy': 'http://localhost:5000',
+        '/ws-proxy': { target: 'ws://localhost:5000', ws: true },
       },
-      server: {
-        proxy: {
-          //Target your Node.js backend
-          '/api-proxy': 'http://localhost:5000',
-          '/ws-proxy': {target: 'ws://localhost:5000', ws: true},
-        },
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       },
-      plugins: react(),
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+    },
+  };
 });
